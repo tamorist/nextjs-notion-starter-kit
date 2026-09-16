5c7694728cdef678574395b25a3dcf3d5b48ecba
tamorist <tamori24@gmail.com>
2026-09-16 19:42:04 +0900

    Update site.config.ts
    Setting environment variables, etc.

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3da0f0df2799802e82c9fa1839c55160',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Planetary-OS',
+  domain: 'planetary-os.org',
+  author: 'Tatsuya Mori',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
